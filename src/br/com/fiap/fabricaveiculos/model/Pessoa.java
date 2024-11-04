fe6d7fa9a6c1eb84486af75872331ede72d53cac
thibasBigT <thiago.temoteo1@gmail.com>
2024-11-04 16:49:14 -0300

    add: nova classe adicionada

--- src/br/com/fiap/fabricaveiculos/model/Pessoa.java
+++ src/br/com/fiap/fabricaveiculos/model/Pessoa.java
@@ -5,7 +5,7 @@ abstract class Pessoa {
 	protected String cpf;
 	protected String endereco;
 	protected int idade;
-	
+
 	public Pessoa(String nome, String cpf, String endereco, int idade) {
 		super();
 		this.nome = nome;
@@ -13,38 +13,44 @@ abstract class Pessoa {
 		this.endereco = endereco;
 		this.idade = idade;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
 	}
+
 	public int getIdade() {
 		return idade;
 	}
+
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-	
+
 	public void exibePessoa() {
 		System.out.println("nome: " + this.nome);
 		System.out.println("idade: " + this.idade);
 		System.out.println("cpf: " + this.cpf);
 		System.out.println("Endereco: " + this.endereco);
 	}
-	
-	
+
 }
